69eaf76f55203e5dd7318ec83ec90287619d0fff
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-10-16 14:01:27 -0400

    Add additional tests to check for standard deviation for multiple data sets

--- spec/standarddev_spec.rb
+++ spec/standarddev_spec.rb
@@ -12,5 +12,8 @@ RSpec.describe StandardDev do
     standarddev = StandardDev.new
 
     expect(standarddev.standard_deviation([26, 11, 29, 31, 21, 11])).to eq(8.80)
+    expect(standarddev.standard_deviation([23, 20, 14, 35, 28])).to eq(7.97)
+    expect(standarddev.standard_deviation([17, 22, 40, 54, 73, 101])).to eq(31.97)
+    expect(standarddev.standard_deviation([1175, 1189, 1229, 1349, 1398, 1450])).to eq(116.17)
   end
 end
