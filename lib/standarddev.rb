fa6b321b9b740cc378cba6a5dfd368d74d821b3f
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-10-16 14:01:45 -0400

    Complete 'standard_deviation' method

--- lib/standarddev.rb
+++ lib/standarddev.rb
@@ -4,6 +4,24 @@ class StandardDev
   end
 
   def standard_deviation(set)
-    
+    sum_of_set = set.sum
+    mean = sum_of_set.to_f / set.count
+    deviation_list = []
+    square_list = []
+
+    set.each do |s|
+      deviation = s - mean
+      deviation_list << deviation
+    end
+    deviation_list.each do |deviation|
+      square = deviation * deviation
+      square_list << square
+    end
+    square_list_sum = square_list.sum
+
+    closer = square_list_sum / (set.count - 1)
+    final = Math.sqrt(closer)
+
+    final.round(2)
   end
 end
